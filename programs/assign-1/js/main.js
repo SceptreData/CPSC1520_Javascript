91b85a4f79a0b83cca8b9a70fa63816559b83a98
David Bergeron <dbergeron2@nait.ca>
2019-02-11 09:54:11 -0700

    finished assignment 1

--- programs/assign-1/js/main.js
+++ programs/assign-1/js/main.js
@@ -1,9 +1,9 @@
 /*
-    The Meme Generator
+The Meme Generator
 
-    David Bergeron
-    david.f.bergeron@gmail.com
-    ------------------------------*/
+David Bergeron
+david.f.bergeron@gmail.com
+------------------------------*/
 
 const placeHolder = document.querySelector('img').src;
 const memeFiles = [
@@ -16,7 +16,7 @@ const memeFiles = [
 /* The FORM */
 const theForm = document.querySelector('.meme-form');
 
-/* The Elements we want to Control. */
+/* The Elements we want to control. */
 const imgElt = document.querySelector('.meme-display img');
 const topText = document.querySelector('.top-text');
 const bottomText = document.querySelector('.bottom-text');
@@ -24,80 +24,88 @@ const bottomText = document.querySelector('.bottom-text');
 /* The ugly list of errors */
 const errorList = document.querySelector('.error');
 
+
 /* Build a path string for our meme files */
-function getMemePath(idx){
+function getMemePath(idx) {
     return 'img/' + memeFiles[idx - 1];
 }
 
-function setImage(memeIdx, altStr){
-    if (memeIdx > 0){
-        imgElt.src = getMemePath(memeIdx);
-        imgElt.alt = altStr;
-    } else {
+/* Set our Meme Image*/
+function setImage(memeIdx, altStr) {
+    /* If our Meme is the placeholder */
+    if (memeIdx === 0) {
         imgElt.src = placeHolder;
-        imgElt.alt = 'Placeholder Image'
+    /* Otherwise, set the appropriate meme */
+    } else {
+        imgElt.src = getMemePath(memeIdx);
     }
+    imgElt.alt = altStr;
 }
 
-
-function setTopText(str){
+function setTopText(str) {
     topText.innerHTML = str;
 }
 
-function setBottomText(str){
+function setBottomText(str) {
     bottomText.innerHTML = str;
 }
 
-function addError(err){
+function addError(err) {
     errorList.innerHTML += "<li>Error: " + err + "</li>";
 }
 
-function clearErrors(){
-    errorList.innerHTML  = '';
+function clearErrors() {
+    errorList.innerHTML = '';
 }
 
-function isEmptyOrWhiteSpace(str){
+function isEmptyOrWhiteSpace(str) {
     return !str || !str.trim();
 }
 
 
-theForm.addEventListener('submit', event=> {
+/* Change our Meme Image whenever a new drop down is selected. */
+theForm.addEventListener('change', event => {
     let meme = theForm.elements[0];
     let memeIdx = meme.selectedIndex;
 
-    // Build our Meme String with a regex (ooooh)
-    let memeStr = meme.value.replace('/-/g', ' ');
+    if (memeIdx === 0) {
+        setImage(0, 'Placeholder Image');
+    } else {
+        // Use a regex to replace dashes with spaces
+        let memeStr = meme.value.replace(/-/g, ' ');
+        setImage(memeIdx, memeStr);
+    }
+});
+
+/* Generate the text for our Meme and check for errors */
+theForm.addEventListener('submit', event => {
+    clearErrors();
 
     let topStr = theForm.elements[1].value;
     let botStr = theForm.elements[2].value;
 
     let isValid = true;
 
-    if (memeIdx == 0) {
-        addError("Must select a meme image.");
-        setImage(0);
-        isValid = false;
-    }
-
     if (isEmptyOrWhiteSpace(topStr)) {
         addError("Top Text cannot be empty");
         isValid = false;
     }
 
-    if (isEmptyOrWhiteSpace(botStr)){
+    if (isEmptyOrWhiteSpace(botStr)) {
         addError("Bottom Text cannot be empty");
         isValid = false;
     }
 
-    if (isValid){
-        setImage(memeIdx, memeStr);
+    if (isValid) {
         setTopText(theForm.elements[1].value);
         setBottomText(theForm.elements[2].value);
+        clearErrors();
     }
     event.preventDefault();
 });
 
-theForm.addEventListener('reset', event=>{
+/* Reset our forms */
+theForm.addEventListener('reset', event => {
     setImage(0);
     setTopText('');
     setBottomText('');
