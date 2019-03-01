222580c63d486791ba97a0dc352c9480e7925021
David Bergeron <dbergeron2@nait.ca>
2019-03-01 09:52:47 -0700

    javascript search form

--- programs/todo_list/js/main.js
+++ programs/todo_list/js/main.js
@@ -1,8 +1,15 @@
 // Enter JavaScript for the exercise here...
 let selectAll = document.querySelector('[name=toggle]');
-
 let options = document.querySelectorAll('[name=message]');
 
+let input = document.querySelector('#userInput');
+let submit = document.querySelector('#formSubmit');
+let form = document.querySelector('form');
+
+let contentList = document.querySelectorAll('td');
+
+console.log(input.value);
+console.log(submit);
 selectAll.addEventListener('change', e=>{
     for (const opt of options){
         if (e.target.checked) {
@@ -12,3 +19,19 @@ selectAll.addEventListener('change', e=>{
         }
     }
 });
+
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    console.log(input.value);
+
+    let searchVal = input.value.toUpperCase();
+
+    for (let i = 1; i < contentList.length; i += 2){
+        let txt = contentList[i].innerText.toUpperCase();
+
+        if (txt.indexOf(searchVal) > -1){
+            contentList[i].classList.add('highlight');
+        }
+    }
+});
+
