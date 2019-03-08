b02dd7121b89303ff1a4653c1736582283e969a8
David Bergeron <dbergeron2@nait.ca>
2019-03-08 09:27:15 -0700

    finished assignment 2

--- programs/assign-2/js/main.js
+++ programs/assign-2/js/main.js
@@ -1,1 +1,42 @@
-// Enter your JavaScript for the solution here...
+/*
+ *      Assignment 2
+ *      David Bergeron
+ */
+
+let input = document.querySelector('.frm-control');
+let reset = document.querySelector('.reset');
+let images = document.querySelectorAll('.thumb-display');
+let tagList = document.querySelectorAll('.tags');
+
+
+function isEmptyOrWhiteSpace(str) {
+    return !str || !str.trim();
+}
+
+
+input.addEventListener('input', ()=> {
+    if (isEmptyOrWhiteSpace(input.value)){
+        reset.classList.add('hidden');
+    } else {
+        reset.classList.remove('hidden');
+    }
+    
+    let searchVal = input.value.toUpperCase();
+    for (let [idx, img] of images.entries()){
+        let imgTags = tagList[idx].innerText.toUpperCase();
+        if (imgTags.includes(searchVal)) {
+            img.classList.remove('hidden');
+        } else {
+            img.classList.add('hidden');
+        }
+    }
+});
+
+
+reset.addEventListener('click', ()=> {
+    reset.classList.add('hidden');
+    input.value = "";
+    for (let img of images){
+        img.classList.remove('hidden');
+    }
+});
