896849a9e9fddb1f41b66fc90aefef72698af0de
David Bergeron <dbergeron2@nait.ca>
2019-04-24 09:24:18 -0600

    pushing jquery lesson

---
+++ programs/jquery/js/main.js
@@ -0,0 +1,6 @@
+// ENSURE YOU HAVE THE CONSOLE OPEN TO VIEW OUTPUT
+// you can put slides examples below or in the console.
+console.log($("p"));
+console.log($(`p:eq(1)`).text());
+
+$(`p:eq(2)`).text("YO WAZZUP");
